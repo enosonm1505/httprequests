446480f46237275992bd09da9b58509404d49696
enoson <enobugbounty@gmail.com>
2021-12-23 20:11:59 +0530

    master

--- hello.py
+++ hello.py
@@ -7,26 +7,29 @@ c=a.readlines()
 for i in c:
     cd=i
     ac=cd.rstrip()
-    r = requests.get("https://"+ac+"/")
-    if '200' in r.content:       
-        print('200'+i)
-        acd=open("output.txt","a")
-        acd.write("\n")
-        acd.write("\n")
-        acd.write("\n")
-        acd.write("Hostname : "+i)
-        acd.write("\n")
-        acd.write("\n")
-        acd.write(r.content)
-    elif '302' in r.content:
-        print('200'+i)
-        acd=open("output.txt","a")
-        acd.write("\n")
-        acd.write("\n")
-        acd.write("\n")
-        acd.write("Hostname : "+i)
-        acd.write("\n")
-        acd.write("\n")
-        acd.write(r.content)
-    else:
-        print("something wrong")
+    try:
+        r = requests.get("https://"+ac+"/")
+        if '200' in r.content:       
+            print('200'+i)
+            acd=open("output.txt","a")
+            acd.write("\n")
+            acd.write("\n")
+            acd.write("\n")
+            acd.write("Hostname : "+i)
+            acd.write("\n")
+            acd.write("\n")
+            acd.write(r.content)
+        elif '302' in r.content:
+            print('200'+i)
+            acd=open("output.txt","a")
+            acd.write("\n")
+            acd.write("\n")
+            acd.write("\n")
+            acd.write("Hostname : "+i)
+            acd.write("\n")
+            acd.write("\n")
+            acd.write(r.content)
+        else:
+            print("something wrong")
+    except requests.exceptions.ConnectionError:
+        print("error")
